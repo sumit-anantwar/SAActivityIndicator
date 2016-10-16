c88860f89825ff9b45ba9d4435590341f840fead
Sumit Anantwar <sumit.anantwar@gmail.com>
2016-10-16 22:15:37 +0300

    Initial Commit

---
+++ library/src/main/java/com/sumitanantwar/sa_activityindicator/SAActivityIndicator.java
@@ -0,0 +1,27 @@
+package com.sumitanantwar.sa_activityindicator;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Leo on 10/16/2016.
+ */
+
+public class SAActivityIndicator extends RelativeLayout
+{
+    public SAActivityIndicator(Context context)
+    {
+        super(context);
+    }
+
+    public SAActivityIndicator(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public SAActivityIndicator(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+    }
+}
