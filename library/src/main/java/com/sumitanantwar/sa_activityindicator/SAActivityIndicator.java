f579065844f7d7a706dc5974e0b64f4c846b8595
Sumit Anantwar <sumit.anantwar@gmail.com>
2016-10-17 01:56:17 +0300

    Pulsating Indicator

--- library/src/main/java/com/sumitanantwar/sa_activityindicator/SAActivityIndicator.java
+++ library/src/main/java/com/sumitanantwar/sa_activityindicator/SAActivityIndicator.java
@@ -1,6 +1,8 @@
 package com.sumitanantwar.sa_activityindicator;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.widget.RelativeLayout;
 
@@ -10,18 +12,39 @@ import android.widget.RelativeLayout;
 
 public class SAActivityIndicator extends RelativeLayout
 {
+    public enum ActivityIndicatorType
+    {
+        PULSATING(0),
+        SPINNER(1);
+
+        public final int index;
+
+        ActivityIndicatorType(int index) { this.index = index;}
+    }
+
+    private static final int ACTIVITY_INDICATOR_COLOR = Color.rgb(100, 100, 100);
+
     public SAActivityIndicator(Context context)
     {
-        super(context);
+        this(context, null);
     }
 
     public SAActivityIndicator(Context context, AttributeSet attrs)
     {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public SAActivityIndicator(Context context, AttributeSet attrs, int defStyleAttr)
     {
         super(context, attrs, defStyleAttr);
+
+        final TypedArray attributes =  context.getTheme().obtainStyledAttributes(attrs, R.styleable.SAActivityIndicator, defStyleAttr, 0);
+        initWithAttributes(attributes);
+        attributes.recycle();
+    }
+
+    private void initWithAttributes(TypedArray attributes)
+    {
+
     }
 }
