eef45051aa4bda9b0fba4a222ef35ff343ba199e
Sumit Anantwar <sumit.anantwar@gmail.com>
2016-10-18 14:24:47 +0300

    Pulsating Indicator Complete Spiner Indicator WIP

--- example/src/main/java/com/sumitanantwar/sa_activityindicator_example/MainActivity.java
+++ example/src/main/java/com/sumitanantwar/sa_activityindicator_example/MainActivity.java
@@ -2,12 +2,44 @@ package com.sumitanantwar.sa_activityindicator_example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.sumitanantwar.sa_activityindicator.ActivityIndicatorType;
+import com.sumitanantwar.sa_activityindicator.SAActivityIndicator;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SAActivityIndicator pulseIndicator = (SAActivityIndicator) findViewById(R.id.Pulse);
+        pulseIndicator.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (pulseIndicator.isAnimating())
+                    pulseIndicator.stop();
+                else
+                    pulseIndicator.start();
+            }
+        });
+
+        final SAActivityIndicator spinIndicator = (SAActivityIndicator) findViewById(R.id.Spin);
+        spinIndicator.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (spinIndicator.isAnimating())
+                    spinIndicator.stop();
+                else
+                    spinIndicator.start();
+            }
+        });
     }
 }
--- library/src/main/java/com/sumitanantwar/sa_activityindicator/SAActivityIndicator.java
+++ library/src/main/java/com/sumitanantwar/sa_activityindicator/SAActivityIndicator.java
@@ -3,9 +3,16 @@ package com.sumitanantwar.sa_activityindicator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.graphics.drawable.shapes.Shape;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
 /**
@@ -19,9 +26,15 @@ public class SAActivityIndicator extends RelativeLayout
     private static final int DEFAULT_INDICATOR_TYPE_INDEX       = 0;
     private static final float DEFAULT_INDICATOR_SIZE           = 100;
 
-    private ActivityIndicatorType indicatorType;
-    private int indicatorColor;
-    private float indicatorSize;
+    private FrameLayout container;
+
+    private Context mContext;
+    private Animation mAnimation;
+    private ActivityIndicatorType mActivityIndicatorType;
+    private int mIndicatorColor;
+    private float mIndicatorSize;
+
+    private boolean isAnimating = false;
 
     public SAActivityIndicator(Context context)
     {
@@ -37,28 +50,116 @@ public class SAActivityIndicator extends RelativeLayout
     {
         super(context, attrs, defStyleAttr);
 
+        mContext = context;
+
+        // Add the Main Container
+        addContainer();
+
+        // Initialize with Attributes
         final TypedArray attributes =  context.getTheme().obtainStyledAttributes(attrs, R.styleable.SAActivityIndicator, defStyleAttr, 0);
         initWithAttributes(attributes);
         attributes.recycle();
 
+        //
         initialize();
+    }
+
+    private void addContainer()
+    {
+        container = new FrameLayout(mContext);
+//        container.setBackgroundColor(mIndicatorColor);
+        addView(container);
+
+        RelativeLayout.LayoutParams containerLayoutParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        containerLayoutParams.addRule(CENTER_IN_PARENT, RelativeLayout.TRUE);
+        container.setLayoutParams(containerLayoutParams);
     }
 
     private void initWithAttributes(TypedArray attributes)
     {
-        indicatorType   = ActivityIndicatorType.getEnumFromIndex(attributes.getInt(R.styleable.SAActivityIndicator_activity_indicator_type, DEFAULT_INDICATOR_TYPE_INDEX));
-        indicatorColor  = attributes.getColor(R.styleable.SAActivityIndicator_activity_indicator_color, DEFAULT_ACTIVITY_INDICATOR_COLOR);
-        indicatorSize   = attributes.getDimension(R.styleable.SAActivityIndicator_activity_indicator_size, DEFAULT_INDICATOR_SIZE);
+        mIndicatorColor = attributes.getColor(R.styleable.SAActivityIndicator_activity_indicator_color, DEFAULT_ACTIVITY_INDICATOR_COLOR);
+        mIndicatorSize = attributes.getDimension(R.styleable.SAActivityIndicator_activity_indicator_size, DEFAULT_INDICATOR_SIZE);
+
+        setActivityIndicatorType(ActivityIndicatorType.getEnumFromIndex(attributes.getInt(R.styleable.SAActivityIndicator_activity_indicator_type, DEFAULT_INDICATOR_TYPE_INDEX)));
     }
 
     private void initialize()
     {
 
+
     }
 
-    private void generatePulsatingIndicator()
+    private View pulsatingIndicatorView()
     {
-        Shape circle = new OvalShape();
+        GradientDrawable background = new GradientDrawable();
+        background.setShape(GradientDrawable.OVAL);
+        background.setColor(mIndicatorColor);
 
+        View view = new View(mContext);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams((int)mIndicatorSize, (int)mIndicatorSize);
+        view.setLayoutParams(params);
+        view.setBackgroundDrawable(background);
+
+        return view;
+    }
+
+    private View spinnerIndicatorView()
+    {
+        GradientDrawable background = new GradientDrawable();
+        background.setShape(GradientDrawable.RECTANGLE);
+        background.setColor(mIndicatorColor);
+
+        View view = new View(mContext);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams((int)mIndicatorSize, (int)mIndicatorSize);
+        view.setLayoutParams(params);
+        view.setBackgroundDrawable(background);
+
+        return view;
+    }
+
+    public void setActivityIndicatorType(ActivityIndicatorType indicatorType)
+    {
+        // Empty the container, if it already contains an Activity Indicator
+        if (container.getChildCount() > 0) container.removeAllViews();
+
+        mActivityIndicatorType = indicatorType;
+        switch (mActivityIndicatorType)
+        {
+            case PULSATING:
+                container.addView(pulsatingIndicatorView());
+                break;
+
+            case SPINNER:
+                container.addView(spinnerIndicatorView());
+                break;
+        }
+    }
+
+    public void start()
+    {
+        switch (mActivityIndicatorType)
+        {
+            case PULSATING:
+                mAnimation = AnimationUtils.loadAnimation(mContext, R.anim.pulse);
+                break;
+
+            case SPINNER:
+                mAnimation = AnimationUtils.loadAnimation(mContext, R.anim.spin);
+                break;
+        }
+
+        isAnimating = true;
+        container.startAnimation(mAnimation);
+    }
+
+    public void stop()
+    {
+        isAnimating = false;
+        container.clearAnimation();
+    }
+
+    public boolean isAnimating()
+    {
+        return isAnimating;
     }
 }
